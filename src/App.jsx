0cb0d2a0bba74ee99ed9f854beafaff5cea0298d
Ashot <ashotisraelyan@gmail.com>
2024-03-27 11:59:21 +0400

    feat: hero section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import Nav from "./components/Nav";
-import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
-const App = () => (
-    <main className="relative">
+const App = () => {
+  return (
+    <main className='relative'>
       <Nav />
-      <section className="xl:padding-1 wide:padding-r padding-b">
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <PopularProducts />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SuperQuality />
       </section>
-      <section className="padding-x py-10">
+      <section className='padding-x py-10'>
         <Services />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className="bg-pale-blue padding">
+      <section className='bg-pale-blue padding'>
         <CustomerReviews />
       </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="bg-black padding-x padding-t pb-8">
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
     </main>
-    );
+  );
+};
 
 export default App;
